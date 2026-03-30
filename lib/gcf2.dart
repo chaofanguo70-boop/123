8577652d43aaea36044ccebe764d478120f0efdd
123 <https://github.com/chaofanguo70-boop/123.git>
2026-03-30 14:06:29 +0900

    start

---
+++ lib/gcf2.dart
@@ -0,0 +1,9 @@
+void main() {
+  int num1 = 10;
+  double pi = 3.14;
+
+  double num3 = num1 * pi;
+  num num4 = num1 * pi;
+   print("num3: $num4");
+  print("num4: $num4");
+}
---
+++ lib/gcf3.dart
@@ -0,0 +1,10 @@
+void main() {
+  String firstName = "Guochaofan";
+  String surname = "Hong";
+
+  String fullname = "$surname $firstName";
+
+  print("length : ${fullname.length}");
+  print("hello :$firstName");
+  print("$fullname : ${fullname.length}");
+}
